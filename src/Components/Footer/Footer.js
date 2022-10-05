f42050922fc663394fa2de1790526cecd8044f63
Nhibel <45295528+Nhibel@users.noreply.github.com>
2022-10-05 13:06:09 +0200

    Rearrange display menus

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -36,55 +36,61 @@ export default function Footer() {
         className="justify-content-center contact-navlink footer fixed-bottom"
         expand="lg"
       >
-        <Nav>
-          <Nav.Link
-            href="https://www.instagram.com/nekoayume/"
-            target="_blank"
-            className="px-2 py-1"
-          >
-            <img src={instagram} alt="instagram" className="contact-icon" />
-          </Nav.Link>
-
-          <Nav.Link
-            href="https://fr.linkedin.com/in/aur%C3%A9lia-bezfamille-03208321"
-            target="_blank"
-            className="px-2 py-1"
-          >
-            <img src={linkedin} alt="linkedin" className="contact-icon" />
-          </Nav.Link>
-
-          <Nav.Link as={Link} to="/contact" className="px-2 py-1">
-            <img src={mail} alt="mail" className="contact-icon" />
-          </Nav.Link>
-
-          <Nav.Link href="tel:0631684770" className="px-2 py-1">
-            <img src={phone} alt="phone" className="contact-icon" />
-          </Nav.Link>
-
-          <Nav.Link
-            href="tel:0631684770"
-            className="pt-1 pb-1 pl-0 pr-2 call-me"
-          >
-            Me contacter : 06 31 68 47 70
-          </Nav.Link>
-
-          {showAdmin && (
-            <Nav.Link as={Link} to="/admin" className="px-2 py-1">
-              ADMIN
-            </Nav.Link>
-          )}
-
-          {showAdmin && (
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-center"
+        >
+          <Nav>
             <Nav.Link
+              href="https://www.instagram.com/nekoayume/"
+              target="_blank"
               className="px-2 py-1"
-              onClick={() => {
-                logOut();
-              }}
             >
-              LOGOUT
+              <img src={instagram} alt="instagram" className="contact-icon" />
             </Nav.Link>
-          )}
-        </Nav>
+
+            <Nav.Link
+              href="https://fr.linkedin.com/in/aur%C3%A9lia-bezfamille-03208321"
+              target="_blank"
+              className="px-2 py-1"
+            >
+              <img src={linkedin} alt="linkedin" className="contact-icon" />
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/contact" className="px-2 py-1">
+              <img src={mail} alt="mail" className="contact-icon" />
+            </Nav.Link>
+
+            <Nav.Link href="tel:0631684770" className="px-2 py-1">
+              <img src={phone} alt="phone" className="contact-icon" />
+            </Nav.Link>
+
+            <Nav.Link
+              href="tel:0631684770"
+              className="pt-1 pb-1 pl-0 pr-2 call-me"
+            >
+              Me contacter : 06 31 68 47 70
+            </Nav.Link>
+
+            {showAdmin && (
+              <Nav.Link as={Link} to="/admin" className="px-2 py-1">
+                ADMIN
+              </Nav.Link>
+            )}
+
+            {showAdmin && (
+              <Nav.Link
+                className="px-2 py-1"
+                onClick={() => {
+                  logOut();
+                }}
+              >
+                LOGOUT
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
